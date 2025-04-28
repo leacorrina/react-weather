1aeb00b0b68f5e09e722126843c7a2e1398cb013
leacorrina <aculeawhite@gmail.com>
2025-04-27 21:01:43 -0400

    Moved Github link to Header & fixed target for link

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
+        <h3>
+          Coded by Lea White and{" "}
+          <a href="https://github.com/leacorrina/react-weather" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </h3>
       </header>
-      <h3>
-        Coded by Lea White and{" "}
-        <a href="https://github.com/leacorrina/react-weather" target="">
-          open-sourced on GitHub
-        </a>
-      </h3>
     </div>
   );
 }
