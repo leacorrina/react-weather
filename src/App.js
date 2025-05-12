ecf467ef6fecc826572b6adc4181b39105692d11
leacorrina <aculeawhite@gmail.com>
2025-05-11 23:14:18 -0400

    cleaned up React files & added meta description

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <Weather />
-        <h3>
-          Coded by Lea White and{" "}
-          <a
-            href="https://github.com/leacorrina/react-weather"
-            rel="noreferrer"
-            target="_blank"
-          >
-            open-sourced on GitHub
-          </a>
-        </h3>
-      </header>
+      <h1>Weather App</h1>
+      <Weather />
+      <footer>
+        Coded by Lea White and{" "}
+        <a
+          href="https://github.com/leacorrina/react-weather"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
